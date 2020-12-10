4cdcf5799b8323eec4e4b640591bcd66e367f350
zigiagozal <vita.vita8520@gmail.com>
2020-12-10 09:52:57 +0200

    make it better

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var http = require('http');
 var express = require("express")
 var app=express()
 var mongoose= require('mongoose')
@@ -8,33 +9,52 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+
 //open the html homepage as a main coment
-app.get('/',function(req, res, next){
-    fs.readFile("C:\Users\zigiagozal\Desktop\school\asaf\project\project\homePage.html","UTF-8",function(err, contents){
+app.get('/', function(req, res, next){
+    //res.json("kk")
+    //cant read the file------------------------------
+    fs.readFile("C:\\Users\\zigiagozal\\Desktop\\school\\asaf\\project\\project\\homePage.html","UTF-8",function(err, contents){
         res.json(contents);
     })
 })
 
 
-app.get('/insert', function(req, res, next){
-    var p1 = {
-    firstname:"first name",
-    larstname:"last name",
-    gmail:"String",
-    nickname:"String",
-    gender:"String",
-    password:"String",
-    created: new Date()
-    };
 
-    var newItem = new CurrentProduct(p1)
-    newItem.save(function(err, item){
-        if(err){
-            next(err);
-        }
-        res.json({usersModel:newItem});
-    });//end of save
-});
+
+
+
+//open the rules of the game page
+// app.get('/rulesOfTheGame', function(req, res, next){
+//     res.json("the rules")
+//     fs.readFile("C:\Users\zigiagozal\Desktop\school\asaf\project\project\rulesOfTheGamePage.html","UTF-8",function(err, contents){
+//         res.json(contents);
+//     })
+// })
+
+
+// app.get('/insert', function(req, res, next){
+//     var p1 = {
+//     firstname:"first name",
+//     larstname:"last name",
+//     gmail:"String",
+//     nickname:"String",
+//     gender:"String",
+//     password:"String",
+//     created: new Date()
+//     };
+
+//     var newItem = new CurrentProduct(p1)
+//     newItem.save(function(err, item){
+//         if(err){
+//             next(err);
+//         }
+//         res.json({usersModel:newItem});
+//     });//end of save
+// });
+
+
+
 
 var strConnect ="mongodb+srv://zigiagozal:vita8520@cluster0.xvfbv.mongodb.net/usersModel?retryWrites=true&w=majority"
 
@@ -43,5 +63,5 @@ mongoose.connect(strConnect, OPT);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
-console.log("Running om port " + port);
+    console.log("Running om port " + port);
 })
